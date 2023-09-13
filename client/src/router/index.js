2771c0c8c743c5b7e7a4e155087ab6b2749bcbf3
tim niehenke <tim.niehenke@gmail.com>
2023-09-13 15:06:49 +0200

    Results Component beerdigt

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Ping from '../components/Ping.vue'
 import SearchBox from '../components/SearchBox.vue'
-import Results from '../components/Results.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,11 +14,6 @@ const router = createRouter({
       path: '/search',
       name: 'search',
       component: SearchBox
-    },
-    {
-      path: '/results',
-      name: 'results',
-      component: Results
     }
   ]
 })
